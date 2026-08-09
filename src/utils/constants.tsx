9fdebefa1ddf0a6c719184776bfc818ccb58c08d
dannyk-dev <kruger.dkk@gmail.com>
2026-08-09 20:04:49 -0300

    updates

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -18,7 +18,7 @@ export const navigationLinks: TNavigationLink[] = [
   { _key: "nav-home", label: "Início", href: "/" },
   { _key: "nav-about", label: "Sobre", href: "/sobre" },
   { _key: "nav-services", label: "Atuação", href: "/servicos" },
-  { _key: "nav-pricing", label: "Honorários", href: "/honorarios" },
+  // { _key: "nav-pricing", label: "Honorários", href: "/honorarios" },
   { _key: "nav-faq", label: "Dúvidas", href: "/duvidas" },
   { _key: "nav-blog", label: "Artigos", href: "/artigos" },
 ];
