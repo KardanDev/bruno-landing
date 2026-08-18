54ee0bcdce5e3d122ee7c15814bd19aab27950de
dannyk-dev <kruger.dkk@gmail.com>
2026-08-18 13:08:42 -0300

    feature: added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { getSettings } from "@/sanity/lib/content";
 import { createMetadata } from "@/sanity/lib/metadata";
 
 import NavDock from "@/components/site/nav-dock";
+import Script from "next/script";
 
 export async function generateMetadata(): Promise<Metadata> {
   return createMetadata(undefined, await getSettings());
@@ -21,11 +22,30 @@ export default async function RootLayout({
   return (
     <html lang="pt-BR">
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PGCRJ4QC"
+            height="0"
+            width="0"
+            style={{
+              display: "none",
+              visibility: "hidden",
+            }}
+          />
+        </noscript>
         <Provider>
           <NavDock settings={settings} />
           <main>{children}</main>
           <SiteFooter settings={settings} />
         </Provider>
+
+        <Script id="google-tag-manager" strategy="afterInteractive">{`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PGCRJ4QC');
+        `}</Script>
       </body>
     </html>
   );
